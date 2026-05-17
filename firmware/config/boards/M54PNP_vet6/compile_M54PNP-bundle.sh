717c62c719741f1644bf8b0dafb745b008903387
TurboMarian <73337486+TurboMarian@users.noreply.github.com>
2026-05-17 14:45:58 +0100

    M54PNP_vet6 board

---
+++ firmware/config/boards/M54PNP_vet6/compile_M54PNP-bundle.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd ../../.. && bash bin/compile.sh -b config/boards/M54PNP_vet6/meta-info.env
---
+++ firmware/config/boards/M54PNP_vet6/compile_M54PNP.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd ../../.. && bash bin/compile.sh config/boards/M54PNP_vet6/meta-info.env
